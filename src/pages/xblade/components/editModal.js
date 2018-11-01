43ef79198e80a028974179fc004ba549dcb95861
NiShiAn <30385099+NiShiAn@users.noreply.github.com>
2018-11-01 19:07:40 +0800

    日常提交

--- src/pages/xblade/components/editModal.js
+++ src/pages/xblade/components/editModal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, Modal } from 'antd'
+import { Col, Form, Input, Modal } from 'antd'
 
 const editModal = ({
     form: {
@@ -11,27 +11,41 @@ const editModal = ({
     ...mPros
 }) => {
     const { info } = mPros;
+    var fetter = info.Fetter.split('/').length > 1 ? info.Fetter.split('/')[1] : ""
+    var target = info.Fetter.split('/')[0]
     const formItemLayout = {
         labelCol: {
-          span: 6,
+          span: 4,
         },
         wrapperCol: {
-          span: 14,
+          span: 20,
         },
-      }
+    }
       
     return (
         <Modal {...mPros}>
-            <Form layout="horizontal">
-                <Form.Item label="Name" hasFeedback {...formItemLayout}>
-                    {getFieldDecorator('name', {
-                        initialValue: info.Name,
-                        rules: [{
-                            required: true
-                        }],
-                    })(<Input />)}
+          <Form layout="horizontal">
+            <Form.Item label="羁绊" hasFeedback {...formItemLayout}>
+              <Col span={3}>
+                <Form.Item>
+                  {getFieldDecorator('Fetter',{
+                    initialValue: fetter
+                  })(<Input />)}
+                </Form.Item> 
+              </Col>
+              <Col span={1}>
+                <span style={{ display: 'inline-block' , width: '100%', textAlign: 'center' }}>/</span>
+              </Col>
+              <Col span={3}>
+                <Form.Item>
+                  {getFieldDecorator('Target',{
+                    initialValue: target,
+                    rules: [{ required: true, message: '必填' }]
+                  })(<Input />)}
                 </Form.Item>
-            </Form>
+              </Col> 
+            </Form.Item>
+          </Form>
         </Modal>
     )
 }
